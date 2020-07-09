c143e55793430a8bcc9efce303e7631b4ddcc2bb
passt0r <dima3@ymail.com>
2020-07-09 17:40:11 +0300

    Initial Commit

---
+++ BoostersTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BoostersTest
+//
+//  Created by Dmytro Pasinchuk on 09.07.2020.
+//  Copyright © 2020 Dmytro Pasinchuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
